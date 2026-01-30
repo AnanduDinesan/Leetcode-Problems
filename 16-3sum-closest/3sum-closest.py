150662e0ede0444381b09a37ca1bb58887c042d9
AnanduDinesan <anandudinesan18@gmail.com>
2026-01-30 13:39:10 +0530

    Time: 323 ms (83.04%) | Memory: 19.4 MB (21.75%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -5,6 +5,8 @@ class Solution:
         nums.sort()
 
         for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             l=i+1
             r=len(nums)-1
 
