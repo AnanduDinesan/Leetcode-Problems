a22dae53d0337d0bc0ddef7a1cb54ca653905311
AnanduDinesan <anandudinesan18@gmail.com>
2026-02-02 14:59:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (42.75%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n1=0
+        n2=0
+        for i in num1:
+            n=ord(i)-48
+            n1=n1*10+n
+        for i in num2:
+            n=ord(i)-48
+            n2=n2*10+n
+        print(n1,n2)
+        return str(n1*n2)
